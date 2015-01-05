ab34fbe820cfc692e7a76432fce4edf6afac29db
Kyu Yeon Lee <rebel1324@gmail.com>
2015-01-05 16:07:23 +0900

    Added 3D2D Touch Library. Modified ATM for Example display.

--- entities/entities/nut_atm.lua
+++ entities/entities/nut_atm.lua
@@ -38,38 +38,125 @@ if (SERVER) then
 	end
 
 	function ENT:Use(activator)
-		netstream.Start(activator, "nutBank")
+		--netstream.Start(activator, "nutBank")
 	end
 else
+	local gradient = nut.util.getMaterial("vgui/gradient-u")
+	local commands = {
+		{"deposit", "/bankdeposit"},
+		{"withdraw", "/bankwithdraw"},
+		{"transfer"},
+	}
+
+	local function drawMatrixString(str, font, x, y, scale, angle, color)
+		surface.SetFont(font)
+		local tx, ty = surface.GetTextSize(str)
+
+		local matrix = Matrix()
+		matrix:Translate(Vector(x, y, 1))
+		matrix:Rotate(angle or Angle(0, 0, 0))
+		matrix:Scale(scale)
+
+		cam.PushModelMatrix(matrix)
+			surface.SetTextPos(2, 2)
+			surface.SetTextColor(color or color_white)
+			surface.DrawText(str)
+		cam.PopModelMatrix()
+	end
+
+	local function renderCode(self, ent, w, h)
+		local char = LocalPlayer():getChar()
+
+		if (self.hasFocus) then
+			self.fadeAlpha = math.Clamp(self.fadeAlpha - FrameTime()*4, 0, 1)
+		else
+			self.fadeAlpha = math.Clamp(self.fadeAlpha + FrameTime(), 0, 1)
+		end
+
+		if (char) then
+			local mx, my = self:mousePos()
+			local scale = 1 / self.scale
+
+			if (self.fadeAlpha < 1) then
+				draw.RoundedBox(0, 0, 0, w, h, Color(0, 0, 0, 255))
+				surface.SetDrawColor(33, 33, 33, 255)
+				surface.SetMaterial(gradient)
+				surface.DrawTexturedRect(0, 0, w, h)
+
+				local tx, ty = draw.SimpleText("8", "nutIconsBig", 2 * scale, 2 * scale, ColorAlpha(color_white, 255), 1, 1)
+				tx, ty = draw.SimpleText(char:getReserve(), "nutBigFont", tx + 2 * scale, 2 * scale, ColorAlpha(color_white, 255), 3, 1)
+
+				local bx, by, color
+				self.curSel = -1
+				surface.SetFont("nutBigFont")
+				for i, v in ipairs(commands) do
+					color = color_white
+					tx, ty = surface.GetTextSize(v[1])
+					bx, by = w/2, (6 + (i-1)*3) * scale
+
+					if (self:cursorInBox(bx - tx/2, by - ty/2, tx, ty)) then
+						color = Color(255, 0, 0)
+						self.curSel = i
+					end
+
+					draw.SimpleText(L(v[1]), "nutBigFont", bx, by, ColorAlpha(color, 255), 1, 1)
+				end
+			end
+
+			draw.RoundedBox(0, 0, 0, w, h, Color(0, 0, 0, self.fadeAlpha * 255))
+			draw.SimpleText("ATM", "nutBigFont", w/2, h/2, ColorAlpha(color_white, self.fadeAlpha * 150), 1, 1)
+		end
+	end
+
+	local function onMouseClick(self, key)
+		if (key) then
+			if (self.curSel) then
+				local dat = commands[self.curSel]
+				if (dat and dat[2]) then
+					Derma_StringRequest(
+						L("enterAmount"),
+						L("enterAmount"),
+						0,
+						function(a)
+							LocalPlayer():ConCommand(Format("say %s %s", dat[2], a))
+						end
+					)
+				end
+			end
+		end
+	end
+
+	function ENT:Initialize()
+		self.screen = nut.screen.new(17, 17, .05)
+		
+		self.screen.fadeAlpha = 1
+		self.screen.renderCode = renderCode
+		self.screen.onMouseClick = onMouseClick
+	end
+	
 	function ENT:Draw()
 		self:DrawModel()
 	end
 
-	local pos, ang, renderAng, dist, dFactor
-	local gradient = nut.util.getMaterial("vgui/gradient-u")
-	local w, h, maxDistance = 330, 300, 1024
+	local pos, ang, renderAng
 	function ENT:DrawTranslucent()
+		self.screen:render()
+	end
+
+	function ENT:Think()
 		pos = self:GetPos()
-		dist = LocalPlayer():GetPos():Distance(pos)
-		dFactor = 1 - math.Clamp(dist/maxDistance - .15, 0, 1)
-		if (dist < maxDistance) then
-			ang = self:GetAngles()
+		ang = self:GetAngles()
 
-			pos = pos + ang:Up() * 11.4
-			pos = pos + ang:Right() * -1.2
-			pos = pos + ang:Forward() * 13.2
+		pos = pos + ang:Up() * 11.1
+		pos = pos + ang:Right() * -1.4
+		pos = pos + ang:Forward() * 13.2
 
-			renderAng = Angle(ang[1], ang[2], ang[3])
-			renderAng:RotateAroundAxis(ang:Forward(), 90)
-			renderAng:RotateAroundAxis(ang:Up(), 89)
+		renderAng = Angle(ang[1], ang[2], ang[3])
+		renderAng:RotateAroundAxis(ang:Forward(), 0)
+		renderAng:RotateAroundAxis(ang:Up(), -2)
 
-			cam.Start3D2D(pos, renderAng, .05)
-				draw.RoundedBox(0, -w/2, -h/2, w, h, Color( 0, 0, 0, 255*dFactor ) )
-				surface.SetDrawColor(33, 33, 33, 255*dFactor)
-				surface.SetMaterial(gradient)
-				surface.DrawTexturedRect(-w/2, -h/2, w, h)
-				draw.SimpleText("ATM", "nutBigFont", 0, -h/3*1, ColorAlpha(color_white, 255*dFactor), 1, 1)
-			cam.End3D2D()
-		end
+		self.screen.pos = pos
+		self.screen.ang = renderAng
+		self.screen:think()
 	end
 end
---
+++ schema/libs/sh_touch.lua
@@ -0,0 +1,207 @@
+-- Metasctruct Lua Screen
+-- Metatable Conversion by Black Tea za rebel1324.
+
+nut.screen = nut.screen or {}
+nut.screen.list = nut.screen.list or {}
+
+function nut.screen.new(w, h, scale)
+	local screen = setmetatable(
+		{
+			w = w or 100,
+			h = h or 100,
+			scale = scale or 1,
+		}, 
+		FindMetaTable("TouchScreen"))
+	screen.renderCode = function(scr, ent, wide, tall)
+		print("DEFAULT")
+	end
+	return screen
+end
+
+local _R = debug.getregistry()
+
+local SCREEN = _R.TouchScreen or {}
+SCREEN.__index = SCREEN
+SCREEN.w = 100
+SCREEN.h = 100
+SCREEN.pos = Vector(0, 0, 0)
+SCREEN.ang = Angle(0, 0, 0)
+SCREEN.scale = 1
+SCREEN.filter = true
+SCREEN.screenName = "Default"
+
+function SCREEN:__tostring()
+	return "[SCREEN OBJECT]"
+end
+
+local vec=Vector(0,0,0)
+local function rayQuadIntersect(vOrigin, vDirection, vPlane, vX, vY)
+        local vp = vDirection:Cross(vY)
+
+        local d = vX:DotProduct(vp)
+
+        if (d <= 0.0) then return end
+
+        local vt = vOrigin - vPlane
+        local u = vt:DotProduct(vp)
+        if (u < 0.0 or u > d) then return end
+
+        local v = vDirection:DotProduct(vt:Cross(vX))
+        if (v < 0.0 or v > d) then return end
+
+        return u / d,v / d
+end
+SCREEN.rayQuadIntersect = rayQuadIntersect
+
+function SCREEN:isBehind(client)
+	if ((client:EyePos() - self.pos):DotProduct(self.ang:Forward()) < 0) then
+		return true
+	end
+end
+
+function SCREEN:isAccessible(client)
+	local w, h, pos, ang = self.w, self.h, self.pos, self.ang
+	client = client or LocalPlayer()
+	
+	if (pos:Distance(client:EyePos()) > (w/2 + 350)) then
+		return
+	end
+
+	if ((client:EyePos() - pos):DotProduct(ang:Forward()) < 0) then
+		return
+	end
+
+	local plane = pos
+	+ ang:Up() * h/2
+	+ ang:Right() * (-w/2)
+
+	local x = ang:Right() * w
+	local y = ang:Up() * -h
+
+	return rayQuadIntersect(client:GetShootPos(), client:GetAimVector(), plane, x, y)
+end
+
+if (CLIENT) then
+	SCREEN.mx = -1
+	SCREEN.my = -1
+
+	function SCREEN:render()
+		local pos, ang = self.pos, self.ang
+		local scrDir = EyePos() - pos
+		scrDir:Normalize()
+
+		if (scrDir:DotProduct(ang:Forward()) < 0) then
+			return
+		end
+
+		pos = pos + ang:Right() * (self.w / 2)
+		pos = pos + ang:Up() * (self.h / 2)
+		
+		ang:RotateAroundAxis(ang:Up(), 90)
+		ang:RotateAroundAxis(ang:Forward(), 90)
+
+		local wide = self.w * (1 / self.scale)
+		local tall = self.h * (1 / self.scale)
+
+		cam.Start3D2D(pos, ang, self.scale)		
+			local succ, err = pcall(self.renderCode, self, ent, wide, tall)	
+		cam.End3D2D()
+		
+		if !succ then
+			print(err)
+			return err
+		end
+	end
+
+	function SCREEN:getWide()
+		return self.w * (1 / self.scale)
+	end
+
+	function SCREEN:getTall()
+		return self.h * (1 / self.scale)
+	end
+
+	function SCREEN:mousePos()
+		if (!self.mx) then 
+			return false 
+		end
+
+		return (1 - self.mx) * self:getWide(), self.my * self:getTall() 
+	end
+
+	function SCREEN:cursorInBox(x, y, w, h)
+		local mx, my = self:mousePos()
+
+		if	(mx >= x and mx <= x + w
+			and my >= y and my <= y + h) then
+
+			return true
+		end
+	end
+
+	-- No one should replace this until you know what the hell you're doing. 
+	function SCREEN:think()
+		local client = LocalPlayer()
+		local mx, my = self:isAccessible()
+
+		if mx then
+			self.mx, self.my = mx, my
+			self.hasFocus = true
+			
+			local key = client:KeyDown(bit.bor(IN_USE, IN_ATTACK, IN_ATTACK2))
+
+			if (key and !self.IN_USE) then
+				self.IN_USE = true
+
+				if (self.onMouseClick) then
+					self:onMouseClick(IN_USE)
+				end
+			elseif (!key and self.IN_USE) then
+				self.IN_USE = false
+
+				if (self.onMouseRelease) then
+					self:onMouseRelease(IN_USE)
+				end
+			end
+		else
+			self.hasFocus = nil
+
+			if (self.IN_USE) then
+				self.IN_USE = false
+
+				if (self.onMouseRelease) then
+					self:onMouseRelease(IN_USE)
+				end
+			end
+		end
+	end
+
+	function SCREEN:enableClipping(state)
+		if (state) then
+			local pos = self.pos
+			local up = self.ang:Up()
+			local right = self.ang:Right()
+			local Height = up * self.h*0.5
+			local Width = right * self.w*0.5
+
+			if render.EnableClipping( true ) then
+				self.__bClipping = true
+			else
+				self.__bClipping = false
+			end
+			
+			render.PushCustomClipPlane(up, up:Dot( pos - h ))
+			render.PushCustomClipPlane(-up, (-up):Dot( pos + h ))
+			render.PushCustomClipPlane(right, right:Dot( pos - w ))
+			render.PushCustomClipPlane(-right, (-right):Dot( pos + w ))
+		else
+			render.PopCustomClipPlane()
+			render.PopCustomClipPlane()
+			render.PopCustomClipPlane()
+			render.PopCustomClipPlane()
+			render.EnableClipping( self.__bClipping )
+		end
+	end
+end
+
+_R.TouchScreen = SCREEN

dcd5ca8c6ccaaf6be3bef200cd47af0b9d75d2b5
Kyu Yeon Lee <rebel1324@gmail.com>
2015-01-06 09:02:01 +0900

    Added MLG Nanobot

---
+++ schema/items/drug/sh_mlgbot.lua
@@ -0,0 +1,37 @@
+ITEM.name = "MLG Nanobot"
+ITEM.model = "models/props_lab/box01a.mdl"
+ITEM.desc = "The true secret nanobots for Wreckening NoScoping Action"
+ITEM.duration = 100
+ITEM.attribBoosts = {
+	["gunskill"] = 5,
+}
+
+ITEM:hook("_use", function(item)
+	item.player:EmitSound("items/battery_pickup.wav")
+end)
+
+local weedestColor = Color(150, 25, 0)
+local weedMat = Material("modernrp/hitmarker.png")
+function ITEM:drawEntity(entity, item)
+	entity:SetColor(weedestColor)
+	entity:DrawModel()
+	entity.emitter = entity.emitter or ParticleEmitter(entity:GetPos())
+
+	if (entity.emitter) then
+		if (!entity.nextEmit or entity.nextEmit < CurTime()) then
+			local smoke = entity.emitter:Add(weedMat, entity:GetPos() + entity:OBBCenter())
+			smoke:SetVelocity(VectorRand()*20)
+			smoke:SetDieTime(math.Rand(0.5,1.3))
+			smoke:SetStartAlpha(255)
+			smoke:SetEndAlpha(0)
+			smoke:SetStartSize(math.random(2,4))
+			smoke:SetEndSize(math.random(4,5))
+			smoke:SetRoll(math.Rand(0, 420))
+			smoke:SetRollDelta(math.Rand(-3,3))
+			smoke:SetGravity(Vector( 0, 0, 15))
+			smoke:SetAirResistance(120)
+
+			entity.nextEmit = CurTime() + .4
+		end
+	end
+end

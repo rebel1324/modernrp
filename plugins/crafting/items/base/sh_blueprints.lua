dc1dd7f4de8a7d60d59ad686e299af972175d598
Kyu Yeon Lee <rebel1324@gmail.com>
2015-02-01 12:05:04 +0900

    Change on the crafting

--- plugins/crafting/items/base/sh_blueprints.lua
+++ plugins/crafting/items/base/sh_blueprints.lua
@@ -3,21 +3,22 @@ ITEM.desc = "A blueprint to make another item."
 ITEM.model = "models/props_lab/clipboard.mdl"
 ITEM.width = 1
 ITEM.height = 1
-ITEM.recipe = {
-	
-}
 
 function ITEM:onRegistered()
 	local RECIPE = {}
-	RECIPE.uniqueID = self.uniqueID
-	RECIPE.name = "Test Recipe"
-	RECIPE.desc = "A Test Recipe that does not requires any blueprints."
-	RECIPE.requireditems = ITEM.requireditems
-	RECIPE.resultItems = ITEM.resultItems
-	RECIPE.requireTable = ITEM.requireTable or false
-	RECIPE.removeItems = ITEM.removeItems or true
-	RECIPE.removeItemsOnFail = ITEM.removeItemsOnFail or false
-	RECIPE.blueprint = true
+	if (self.uniqueID) then
+		RECIPE.uniqueID = self.uniqueID
+		RECIPE.name = self.blueprintName
+		RECIPE.desc = self.blueprintDesc
+		RECIPE.requireditems = self.requireditems
+		RECIPE.resultItems = self.resultItems
+		RECIPE.requireTable = self.requireTable or false
+		RECIPE.removeItems = self.removeItems or true
+		RECIPE.removeItemsOnFail = self.removeItemsOnFail or false
+		RECIPE.canCraft = self.canCraft or false
+		RECIPE.onCraft = self.onCraft or false
+		RECIPE.blueprint = true
 
---	nut.item.registerRecipe(self.uniqueID, RECIPE)
+		--nut.item.registerRecipe(self.uniqueID, RECIPE)
+	end
 end
